f37a19711a8dbeeceef6a92b919e3e8c734ba4f7
Jean-Marc L <jean-marc.lienher@bluewin.ch>
2021-05-05 10:08:42 +0200

    m

--- lib/c/mac.h
+++ lib/c/mac.h
@@ -11,19 +11,88 @@
 #else
 #   error "Unknown Apple platform"
 #endif
+#include <stdio.h>
+#include <objc/objc.h>
+#include <objc/runtime.h>
+#include <objc/message.h>
+#include <objc/NSObjCRuntime.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <CoreGraphics/CGBase.h>
+#include <CoreGraphics/CGGeometry.h>
+typedef CGPoint NSPoint;
+typedef CGRect NSRect;
+extern id NSApp;
+extern id const NSDefaultRunLoopMode;
 
 #ifdef JACK_IMPLEMENTATION
 
+int width = 512;
+int height = 256;
+var refresh = 0;
+var key = 0;
+
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0,0,0,1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(
+		GL_TEXTURE_2D,
+		0,
+		3,
+		width,
+		height,
+		0,
+		GL_RGBA,
+		GL_UNSIGNED_BYTE,
+		image32);
+	glLoadIdentity();
+	glBegin(GL_QUADS);
+	glTexCoord2f(0,0); glVertex3f(-1,1, -1);
+	glTexCoord2f(1,0); glVertex3f( 1,1, -1);
+	glTexCoord2f(1,1); glVertex3f( 1, -1, -1);
+	glTexCoord2f(0,1); glVertex3f(-1, -1, -1);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, 0);
+//	SwapBuffers(hDC);
+}
+
+void deInit()
+{
+}
+
+void init()
+{
+
+}
 
 var Screen__refresh() 
 {
-
-		return 0;
+	if (refresh) return 0;
+	refresh = -1;
+	init();
+	return 0;
 }
 
-var Keyboard__screenKeyPressed()
+var Screen__processEvents()
 {
-	return 0;
+	var k = 0;
+	static var in_proc = 0;
+	if (in_proc) {
+		return 0;
+	}
+	in_proc = -1;
+	init();
+	key = 0;
+		if (key) {
+			k = key;
+		}
+	Sys__wait(20);
+	in_proc = 0;
+	return k;
 }
 
 
